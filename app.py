26095a19b8bcfdb212f53b551b31ec9cbd222329
SUNNY BHAVEEN CHANDRA <2016peb5126@mnit.ac.in>
2020-04-09 04:55:13 +0530

    templates added

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request, jsonify
+import os
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
